82ca5cadec2d225ef11b6e42d7f5e71e0e35faec
James Lester <james.lester@shellenergy.co.uk>
2024-07-25 16:42:22 +0100

    update to url

--- secretManagement.py
+++ secretManagement.py
@@ -17,25 +17,25 @@ if  token == '':
   dbutils.notebook.exit("no token provided")
 
 if apiCallType == 'list scopes':
-  response = requests.get('https://shellenergy-prod.cloud.databricks.com/api/2.0/secrets/scopes/list', headers = headers)
+  response = requests.get('https://workspaceName.cloud.databricks.com/api/2.0/secrets/scopes/list', headers = headers)
   print (response.text)
 
 if apiCallType == 'list secrets':
   params = {'scope': scopeValue}
-  response = requests.get('https://shellenergy-prod.cloud.databricks.com/api/2.0/secrets/list', headers = headers, params = params)
+  response = requests.get('https://workspaceName.cloud.databricks.com/api/2.0/secrets/list', headers = headers, params = params)
   print (response.text)
 
 if apiCallType == 'delete scope':
   params = {'scope': scopeValue}
-  response = requests.post('https://shellenergy-prod.cloud.databricks.com/api/2.0/secrets/scopes/delete', headers = headers, params = params)
+  response = requests.post('https://workspaceName.cloud.databricks.com/api/2.0/secrets/scopes/delete', headers = headers, params = params)
   print (response.text)
 
 if apiCallType == 'create scope':
   params = {'scope': scopeValue}
-  response = requests.post('https://shellenergy-prod.cloud.databricks.com/api/2.0/secrets/scopes/create', headers = headers, params = params)
+  response = requests.post('https://workspaceName.cloud.databricks.com/api/2.0/secrets/scopes/create', headers = headers, params = params)
   print (response.text)  
   
 if apiCallType == 'create secret':
   params = {'scope': scopeValue,'key':secretKey,'string_value':secretValue}
-  response = requests.post('https://shellenergy-prod.cloud.databricks.com/api/2.0/secrets/put', headers = headers, params = params)
+  response = requests.post('https://workspaceName.cloud.databricks.com/api/2.0/secrets/put', headers = headers, params = params)
   print (response.text) 
